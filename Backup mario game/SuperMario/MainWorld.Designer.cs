4605ea1ff1be6e5bf9e482564061b1b583aa723e
Lynx Lu <34556826+LynxHack@users.noreply.github.com>
2017-12-14 15:35:11 -0800

    Added GUI

---
+++ Backup mario game/SuperMario/MainWorld.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SuperMario
+{
+    partial class MainWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PlatformTest = new System.Windows.Forms.PictureBox();
+            this.Character = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PlatformTest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Character)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PlatformTest
+            // 
+            this.PlatformTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.PlatformTest.BackColor = System.Drawing.Color.Black;
+            this.PlatformTest.Location = new System.Drawing.Point(295, 249);
+            this.PlatformTest.Name = "PlatformTest";
+            this.PlatformTest.Size = new System.Drawing.Size(116, 114);
+            this.PlatformTest.TabIndex = 1;
+            this.PlatformTest.TabStop = false;
+            // 
+            // Character
+            // 
+            this.Character.BackColor = System.Drawing.Color.Transparent;
+            this.Character.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Character.ErrorImage = null;
+            this.Character.InitialImage = null;
+            this.Character.Location = new System.Drawing.Point(136, 95);
+            this.Character.Name = "Character";
+            this.Character.Size = new System.Drawing.Size(33, 64);
+            this.Character.TabIndex = 0;
+            this.Character.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 25;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // MainWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(644, 360);
+            this.Controls.Add(this.PlatformTest);
+            this.Controls.Add(this.Character);
+            this.Name = "MainWorld";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.PlatformTest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Character)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Character;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox PlatformTest;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
